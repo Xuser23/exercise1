0d988914e3642964570559ccc2bc99c76d04e280
Xuser23 <devamahendrar@gmail.com>
2021-04-18 14:12:16 +0700

    register

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.exercise1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    EditText inEmail, inPass;
+    TextView register;
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inEmail = findViewById(R.id.lEmail);
+        inPass = findViewById(R.id.lPass);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        register = findViewById(R.id.txtReg);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
+
+        public void onClick(View view) {
+            Intent in = new Intent(getApplicationContext(),SignUpActivity.class);
+
+            startActivity(in);
+        }
 }
---
+++ app/src/main/java/com/example/exercise1/SignUpActivity.java
@@ -0,0 +1,15 @@
+package com.example.exercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
